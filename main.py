3d50e72543718cb6b2c5f4667906254ba07a6f77
Sajan Sarker <sajansarker47@gmail.com>
2026-01-24 15:18:04 +0600

    update all the api functions with saving on database and functional on web interface

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import pandas as pd
 from typing import Optional
 
 from datetime import datetime
-from fastapi import FastAPI, templating, HTTPException
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, HttpUrl
 from psycopg2.extras import RealDictCursor
 
@@ -21,6 +23,8 @@ warnings.filterwarnings("ignore")
 load_dotenv.load_dotenv()
 
 app = FastAPI()
+app.mount("/plots", StaticFiles(directory="app/plots"), name="plots")
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
 class CommentRequest(BaseModel):
     comment: str
@@ -29,7 +33,7 @@ class URLRequest(BaseModel):
     url: HttpUrl
 
 @app.on_event('startup')
-def startup_devent():
+def startup_event():
     print("Starting up...")
     global conn, cursor
     global model, tokenizer, sentiment_decoder, type_decoder, device, model_predictor, plots
@@ -56,9 +60,16 @@ def startup_devent():
     plots = PlotResult()
 
 @app.get("/")
-def home():
-    return {"message": "Welcome to the YouTube Comment Sentiment and Type Analysis API"}
-    # return templating.Jinja2Templates(directory="template").TemplateResponse("index.html", {"request": {}})
+def serve_home():
+    return FileResponse("frontend/index.html")
+
+@app.get("/history-page")
+def serve_history():
+    return FileResponse("frontend/history.html")
+
+@app.get("/plots-page")
+def serve_plots():
+    return FileResponse("frontend/plots.html")
 
 @app.post('/predict')
 def predict(comment: CommentRequest):
@@ -74,7 +85,7 @@ def predict(comment: CommentRequest):
     INSERT INTO results 
     (result_type, comment, sentiment, type_, sentiment_confidence, type_confidence, date)
     VALUES (%s, %s, %s, %s, %s, %s, %s)"""
-    values = ('single', comment, sentiment, comment_type, senti_proba, type_proba, date)
+    values = ('comment', comment, sentiment, comment_type, senti_proba, type_proba, date)
 
     try:    
         cursor.execute(query, values)
@@ -113,11 +124,17 @@ def analyze(url: URLRequest):
     df['type'] = df['type'].astype(int).map(lambda x: type_decoder.classes_[x])
 
     plots_dir = 'app/plots'
+
+    sd_name = plots.generate_plot_name('sd')
+    sp_name = plots.generate_plot_name('sp')
+    td_name = plots.generate_plot_name('td')
+    tp_name = plots.generate_plot_name('tp')
+
     plots_path = {
-        'sd' : f"{plots_dir}/{plots.generate_plot_name('sd')}",
-        'sp' : f"{plots_dir}/{plots.generate_plot_name('sp')}",
-        'td' : f"{plots_dir}/{plots.generate_plot_name('td')}",
-        'tp' : f"{plots_dir}/{plots.generate_plot_name('tp')}"
+        'sd' : f"{plots_dir}/{sd_name}",
+        'sp' : f"{plots_dir}/{sp_name}",
+        'td' : f"{plots_dir}/{td_name}",
+        'tp' : f"{plots_dir}/{tp_name}"
     }
 
     # plot the predicted class distributions
@@ -128,12 +145,37 @@ def analyze(url: URLRequest):
     plots.plot_confidence(df, col="sentiment", proba_col="senti_proba", title="Model Confidence per Sentiment Class", xlabel="Confidence Score (%)", ylabel="Sentiment", save_path=plots_path['sp'])
     plots.plot_confidence(df, col="type", proba_col="type_proba", title="Model Confidence per Type Class", xlabel="Confidence Score (%)", ylabel="Type", save_path=plots_path['tp'])
 
+    # store the analysis result in the database
+    url = str(url.url)
+    total_comments = int(len(sentiments))
+    sd = str(f"/plots/{sd_name}")
+    sp = str(f"/plots/{sp_name}")
+    td = str(f"/plots/{td_name}")
+    tp = str(f"/plots/{tp_name}")
+    date = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    query = """
+    INSERT INTO results
+    (result_type, url, total_comments, sentiment_distribution_plot, sentiment_confidence_plot, type_distribution_plot, type_confidence_plot, date)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+    values = ('url', url, total_comments, sd, sp, td, tp, date)
+
+    try:
+        cursor.execute(query, values)
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(
+            status_code=500,
+            detail=f"Database insert failed: {str(e)}"
+        )
+
     return {
-        "total_comments": len(sentiments),
-        "sentiment_distribution_plot": plots_path['sd'],
-        "sentiment_confidence_plot": plots_path['sp'],
-        "type_distribution_plot": plots_path['td'],
-        "type_confidence_plot": plots_path['tp']
+        "total_comments": total_comments,
+        "sentiment_distribution_plot": f"/plots/{sd_name}",
+        "sentiment_confidence_plot": f"/plots/{sp_name}",
+        "type_distribution_plot": f"/plots/{td_name}",
+        "type_confidence_plot": f"/plots/{tp_name}"
     }
 
 @app.get('/history')
